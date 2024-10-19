9eeea46a42ece59b2306b0ec8e10a720bf23faa2
khongbietcode <khongbietcode1@gmail.com>
2024-10-19 22:16:54 +0700

    Thông điệp cam kếts

--- Exercise1/src/main/java/com/mycompany/exercise1/Exercise1_2.java
+++ Exercise1/src/main/java/com/mycompany/exercise1/Exercise1_2.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.exercise1;
 
 /**
  *
  * @author PC 01
  */
-public class Exercise1 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World1!");
-    }
+public class Exercise1_2 {
+    
 }
