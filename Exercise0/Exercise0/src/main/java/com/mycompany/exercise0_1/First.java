84e565c52e9c68fc2496e3e1456938474e65c2bd
khongbietcode <khongbietcode1@gmail.com>
2024-10-19 22:46:42 +0700

    Test2

--- Exercise0/Exercise0/src/main/java/com/mycompany/exercise0_1/First.java
+++ Exercise0/Exercise0/src/main/java/com/mycompany/exercise0_1/First.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.exercise2;
+package com.mycompany.exercise0_1;
 
 /**
  *
  * @author PC 01
  */
-public class Exercise2 {
+public class First {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ Exercise0/Exercise0/src/main/java/exercise0_2/Test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise0_2;
+
+/**
+ *
+ * @author PC 01
+ */
+public class Test {
+    
+}
