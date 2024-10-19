60704f1430b5f65a2e2727ab037bea9dd74722a3
khongbietcode <khongbietcode1@gmail.com>
2024-10-19 22:35:24 +0700

    Test

---
+++ Exercise2_Test/Exercise2/src/main/java/com/mycompany/exercise2/Exercise2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercise2;
+
+/**
+ *
+ * @author PC 01
+ */
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
