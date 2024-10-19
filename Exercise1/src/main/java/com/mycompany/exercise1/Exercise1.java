d21682d0106448b302f6e1819530bc60cabc04a9
khongbietcode <khongbietcode1@gmail.com>
2024-10-19 21:53:33 +0700

    First

---
+++ Exercise1/src/main/java/com/mycompany/exercise1/Exercise1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercise1;
+
+/**
+ *
+ * @author PC 01
+ */
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
