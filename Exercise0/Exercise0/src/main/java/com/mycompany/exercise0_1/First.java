618a5812c9403f8cd6d7a5a706827febe401ab36
khongbietcode <khongbietcode1@gmail.com>
2024-10-20 10:00:25 +0700

    Hello New Day

--- Exercise0/Exercise0/src/main/java/com/mycompany/exercise0_1/First.java
+++ Exercise0/Exercise0/src/main/java/com/mycompany/exercise0_1/First.java
@@ -4,13 +4,35 @@
 
 package com.mycompany.exercise0_1;
 
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author PC 01
  */
-public class First {
+public class First extends JFrame
+    {
+    public First(){
+    setTitle("DemoJFrame");//Tiêu đề JFrame
+    setSize(300,200);//Kích thước của JFrame
+    
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    setDefaultCloseOperation(EXIT_ON_CLOSE);//Thoát chương trình khi click nút exit
+    setLocationRelativeTo(null);//Canh giữa màn hình
+    setResizable(false);//không cho phép thay đổi kích thước JFrame
+//các thuộc tính khác JFrame
+    setLayout(new FlowLayout());
+    JButton button1 = new JButton("Click Me!");
+    JButton button2 = new JButton("Click Me!");
+    add(button1);
+    add(button2);
+ 
+    
     }
-}
+    public static void main(String[]args)
+    {
+    new First().setVisible(true);//Hiển thị JFrame
+    }
+    }
--- Exercise0/Exercise0/src/main/java/exercise0_2/Test.java
+++ Exercise0/Exercise0/src/main/java/exercise0_2/Test.java
@@ -4,10 +4,30 @@
  */
 package exercise0_2;
 
+import javax.swing.JDialog;
+
 /**
  *
  * @author PC 01
  */
-public class Test {
+
+
+    public class Test extends JDialog{
+    public Test()	{
+    setTitle("Demo	JDialog");	//Tiêu đề của hộp thoại JDialog
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);	//Đóng hộp thoại
+    setSize(300,200);
+    setResizable(false);
+   
+    //Các thuộc tính khác
+    }
+    public static void main(String[]	args)	{
+    new Test().setVisible(true);
+    }
+
+    private void setJButton(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    }
     
-}
+
